19213148ce464a26c42fd2b9186001fdc40b4fe8
Carlos Alonzo Lagos <calonzolg@gmail.com>
2020-08-05 21:58:15 -0600

    need config for request the rapidapi

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected $covid_api;
+
+    protected $host;
+
+    protected $key;
+
+    public function __construct()
+    {
+        $this->covid_api = new Client(['base_uri' => config('services.covid_api.uri')]);
+        $this->host = parse_url(config('services.covid_api.uri'), PHP_URL_HOST);
+        $this->key = config('services.covid_api.key');
+    }
 }
---
+++ config/services.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'covid_api' => [
+        'key' => env('COVID_API_KEY_SECRET'),
+        'uri' => env('COVID_API_URI'),
+    ],
+];
